f77aeaa353fc34e48fe6ccd83d1f890987e02cc2
filipw <wojcieszyn@gmail.com>
2016-06-08 20:16:07 +0200

    added a naive port of central route prefix to asp.net core rc2

---
+++ Strathweb.RouteConventions.AspNetCore/src/Strathweb.RouteConventions.AspNetCore/RouteConvention.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using Microsoft.AspNetCore.Mvc.Routing;
+
+namespace Strathweb.RouteConventions.AspNetCore
+{
+    public class RouteConvention : IApplicationModelConvention
+    {
+        private readonly IRouteTemplateProvider _routeTemplateProvider;
+
+        public RouteConvention(IRouteTemplateProvider routeTemplateProvider)
+        {
+            _routeTemplateProvider = routeTemplateProvider;
+        }
+
+        public void Apply(ApplicationModel application)
+        {
+            var centralPrefix = new AttributeRouteModel(_routeTemplateProvider);
+            foreach (var controller in application.Controllers)
+            {
+                var attributeRouteSelectors = controller.Selectors.Where(x => x.AttributeRouteModel != null).ToList();
+                if (attributeRouteSelectors.Any())
+                {
+                    foreach (var selectorModel in attributeRouteSelectors)
+                    {
+                        selectorModel.AttributeRouteModel = AttributeRouteModel.CombineAttributeRouteModel(centralPrefix,
+                            selectorModel.AttributeRouteModel);
+                    }
+                }
+            }
+        }
+    }
+}
